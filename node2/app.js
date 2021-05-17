1e0110832b176201267768645675e25c6455e249
Nikola <nikola@example.com>
2021-05-17 21:52:14 +0000

    proxy_set_header

--- node2/app.js
+++ node2/app.js
@@ -3,5 +3,9 @@ const app = express()
 var os = require("os");
 var hostname = os.hostname();
 
-app.get('/', (req, res) => res.send('Hello World from ' + hostname))
+app.get('/', (req, res) => {
+  console.log(req.headers)
+  res.send('Hello World from ' + hostname)
+})
+
 app.listen(4001, () => console.log('Server ready'))
